178af6cbfefd9d2f1fa1cdf34e53f889869d217f
Koowah <anaslargab@gmail.com>
2022-02-07 00:42:49 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building DNN, RBM & DBN from scratch - training & predicting on MNIST & Binary AlphaDigits - pretraining',
+    author='NasLrgb',
+    license='MIT',
+)
